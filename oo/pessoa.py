c851ac6c0916c2006d1d570811c0f68db148aa36
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-08-26 16:07:43 -0300

    Teste acabou com porcos e passaros passando.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,7 +7,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -17,9 +17,18 @@ class Pessoa:
     def nome_e_atributo_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimenta_da_classe=super().cumprimentar()
+
+        return f'{cumprimenta_da_classe}. Aperto de mão'
+
+class Mutante(Pessoa):
+    olhos = 3
+
 if __name__ == '__main__':
-    renzo = Pessoa(nome='Renzo')
-    luciano = Pessoa(renzo, nome='Luciano')
+    renzo = Mutante(nome='Renzo')
+    luciano = Homem(renzo, nome='Luciano')
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
@@ -37,3 +46,16 @@ if __name__ == '__main__':
     print(renzo.olhos)
     print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
     print(Pessoa.nome_e_atributo_de_classe(), luciano.nome_e_atributo_de_classe())
+
+    pessoa=Pessoa('Anonimo')
+
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+
+    print(isinstance(renzo, Pessoa))
+    print(isinstance(renzo, Homem))
+
+    print(renzo.olhos)
+
+    print(luciano.cumprimentar())
+    print(renzo.cumprimentar())
